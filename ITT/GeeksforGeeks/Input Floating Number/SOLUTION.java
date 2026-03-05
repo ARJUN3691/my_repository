1d1dd910620a21908bcb69200f5a436e8d6f8761
ARJUN3691 <arjunoct005@gmail.com>
2026-03-05 21:54:55 +0530

    Create SOLUTION.java

---
+++ ITT/GeeksforGeeks/Input Floating Number/SOLUTION.java
@@ -0,0 +1,14 @@
+//Back-end complete function Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        // Take a Float input and Print
+        
+        Scanner sc=new Scanner(System.in);
+        double n=sc.nextDouble();
+        System.out.println((n*10));
+        
+    }
+}
